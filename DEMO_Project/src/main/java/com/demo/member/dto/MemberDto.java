275df4b932ecb8c5baedf88ea7ff9871c07252f6
jbn750 <jbn750@gmail.com>
2023-09-12 14:02:56 +0900

    project buildpath

--- DEMO_Project/src/main/java/com/demo/member/dto/MemberDto.java
+++ DEMO_Project/src/main/java/com/demo/member/dto/MemberDto.java
@@ -5,50 +5,56 @@ import java.util.Date;
 //Bean Spec
 public class MemberDto {
 
-	private int no;
-	private String name; 
-	private String email; 
-	private String password;
-	private Date createDate;
-	private Date modifyDate;
+	private int mno;
+	private String id; 
+	private String password; 
+	private String name;
+	private Date nickName;
+	private Date birthDate;
+	private String Phone;
+	private String email;
+	private String address;
+	private Date joinDate;
+	private int emoney;
+	private int point;
+	private Date modDate;
 	
 	public MemberDto() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
-	public MemberDto(int no, String name, String email, String password, Date createDate, Date modifyDate) {
+	public MemberDto(int mno, String id, String password, String name, Date nickName, Date birthDate, String phone,
+			String email, String address, Date joinDate, int emoney, int point, Date modDate) {
 		super();
-		this.no = no;
-		this.name = name;
-		this.email = email;
+		this.mno = mno;
+		this.id = id;
 		this.password = password;
-		this.createDate = createDate;
-		this.modifyDate = modifyDate;
-	}
-
-	public int getNo() {
-		return no;
-	}
-
-	public void setNo(int no) {
-		this.no = no;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
 		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
+		this.nickName = nickName;
+		this.birthDate = birthDate;
+		Phone = phone;
 		this.email = email;
+		this.address = address;
+		this.joinDate = joinDate;
+		this.emoney = emoney;
+		this.point = point;
+		this.modDate = modDate;
+	}
+
+	public int getMno() {
+		return mno;
+	}
+
+	public void setMno(int mno) {
+		this.mno = mno;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
 	}
 
 	public String getPassword() {
@@ -59,29 +65,95 @@ public class MemberDto {
 		this.password = password;
 	}
 
-	public Date getCreateDate() {
-		return createDate;
+	public String getName() {
+		return name;
 	}
 
-	public void setCreateDate(Date createDate) {
-		this.createDate = createDate;
+	public void setName(String name) {
+		this.name = name;
 	}
 
-	public Date getModifyDate() {
-		return modifyDate;
+	public Date getNickName() {
+		return nickName;
 	}
 
-	public void setModifyDate(Date modifyDate) {
-		this.modifyDate = modifyDate;
+	public void setNickName(Date nickName) {
+		this.nickName = nickName;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getPhone() {
+		return Phone;
+	}
+
+	public void setPhone(String phone) {
+		Phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Date getJoinDate() {
+		return joinDate;
+	}
+
+	public void setJoinDate(Date joinDate) {
+		this.joinDate = joinDate;
+	}
+
+	public int getEmoney() {
+		return emoney;
+	}
+
+	public void setEmoney(int emoney) {
+		this.emoney = emoney;
+	}
+
+	public int getPoint() {
+		return point;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+
+	public Date getModDate() {
+		return modDate;
+	}
+
+	public void setModDate(Date modDate) {
+		this.modDate = modDate;
 	}
 
 	@Override
 	public String toString() {
-		return "MemberDto [no=" + no + ", name=" + name + ", email=" + email + ", password=" + password
-				+ ", createDate=" + createDate + ", modifyDate=" + modifyDate + "]";
+		return "MemberDto [mno=" + mno + ", id=" + id + ", password=" + password + ", name=" + name + ", nickName="
+				+ nickName + ", birthDate=" + birthDate + ", Phone=" + Phone + ", email=" + email + ", address="
+				+ address + ", joinDate=" + joinDate + ", emoney=" + emoney + ", point=" + point + ", modDate="
+				+ modDate + "]";
 	}
+
 	
-	
-	
+
 	
 }
